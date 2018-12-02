f1c58e4e3833fe736ab2075ab4912035734dc63f
Александр Саблин <sablin.irk@gmail.com>
2018-12-02 23:19:00 +0800

    v1

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,99 @@
 $(function() {
+	$(".food-carousel").owlCarousel({
+		items: 4,
+		nav: true,
+		loop: true,
+		navText : ["",""]
+	});
 
-	// Пользовательские функции
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		nav: true,
+		loop: true,
+		navText : ["",""]
+	});
+
+
+	// специально делаю без jquery
+
+	let stockTabs = document.querySelectorAll('.promo__tab'),
+		stockLink = document.querySelectorAll('.stock-link');
+
+		for(let i=0; i<stockLink.length;i++){
+			stockLink[i].addEventListener('click', funTabs);
+		}
+		
+
+		function funTabs(e){
+			e.preventDefault();
+			for(let i =0; i<stockLink.length;i++){
+				stockLink[i].classList.remove('active-link');
+			}
+			this.classList.add('active-link');
+			let dataTab = e.target.getAttribute('data-tab');
+			console.log(dataTab);
+			for(let i=0; i < stockTabs.length; i++){
+				if(dataTab == i){
+					stockTabs[i].classList.remove('hide');
+				}else{
+					stockTabs[i].classList.add('hide');
+				}
+			}
+
+		
+		}
+
+		let plus = document.querySelectorAll('.plus')
+			mines = document.querySelectorAll('.mines');
+
+
+		for(let i =0; i < plus.length; i++){
+			plus[i].addEventListener('click', changeNum(1))
+		};
+
+		for(let i =0; i < mines.length; i++){
+			mines[i].addEventListener('click',  changeNum(-1))
+		}
+
+		function changeNum(d){
+			return function(e){
+				e.preventDefault();
+				let num = parseInt(this.parentElement.querySelector('.num').innerHTML);
+				console.log(num)
+				num += d;
+				if(num<=0){
+					num = 1;
+				}
+				this.parentElement.querySelector('.num').innerHTML = num;
+				
+			}
+		
+		}
+
+		let callback = document.querySelector('.callback');
+
+		callback.addEventListener('click', function(){
+			alert('Должно появиться красивое всплывающее окно. Но доделать его не успел. По этому вот пока снеговик: СНЕГОВИК');
+		})
+		
+		
+		
+		let nav = document.querySelector('header nav'),
+		block = document.querySelector('.nav-clone');
+		let box = nav.getBoundingClientRect();
+		let top = box.top + pageYOffset;
+			
+		document.addEventListener('scroll', function(){
+			if(window.pageYOffset > top){
+				nav.classList.add('fixed');
+				block.style.display = 'block';
+			}else{
+				nav.classList.remove('fixed');
+				block.style.display = 'none';
+			}
+
+		})
+
+
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,8 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 gulp.task('scripts', function() {
 	return gulp.src([ // Берем все необходимые библиотеки
 		'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
-		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js', // Берем Magnific Popup
+		'app/libs/OwlCarousel2-2.3.4/dist/owl.carousel.js'
 		])
 		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
 		.pipe(uglify()) // Сжимаем JS файл
